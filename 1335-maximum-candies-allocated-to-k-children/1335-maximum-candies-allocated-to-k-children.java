df666b5bf4b5ddf4a5469d68c422eff12f98412d
aryansharma <aryansharmag29@gmail.com>
2024-04-13 17:08:14 +0530

    Time: 55 ms (11.05%), Space: 63.8 MB (7.89%) - LeetHub

---
+++ 1335-maximum-candies-allocated-to-k-children/1335-maximum-candies-allocated-to-k-children.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int maximumCandies(int[] c, long k) {
+        long sum=0;
+        for(int x:c)
+        sum+=x;
+        if(sum<k)
+        return 0;
+        long s=1;
+        long e=sum;
+        long ans=0;
+        while(s<=e)
+        {
+            long mid=s+(e-s)/2;
+            System.out.println(mid);
+            if(isPossible(c,k,mid))
+            {
+              if(ans<mid)
+              ans=mid;
+              s=mid+1;
+            }
+            else{
+                e=mid-1;
+            }
+        }
+        return (int)ans;
+    }
+    public boolean isPossible(int[]c,long k,long mid)
+    {
+        long children=0;
+        for(int i=0;i<c.length;i++)
+        {
+            children+=(c[i]/mid);
+        }
+        if(children>=k)
+        return true;
+        return false;
+    }
+}
