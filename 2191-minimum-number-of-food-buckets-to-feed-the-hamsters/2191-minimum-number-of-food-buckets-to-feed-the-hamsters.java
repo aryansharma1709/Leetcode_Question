f2f076a724a686d3b6569acd56f33484e3ab5237
aryansharma <aryansharmag29@gmail.com>
2024-04-19 00:00:06 +0530

    Update 2191-minimum-number-of-food-buckets-to-feed-the-hamsters.java

--- 2191-minimum-number-of-food-buckets-to-feed-the-hamsters/2191-minimum-number-of-food-buckets-to-feed-the-hamsters.java
+++ 2191-minimum-number-of-food-buckets-to-feed-the-hamsters/2191-minimum-number-of-food-buckets-to-feed-the-hamsters.java
@@ -1,37 +1,33 @@
 class Solution {
-    public int minimumBuckets(String h) {
-      char[] arr = h.toCharArray();
-        int ans = 0;
-        int n = arr.length;
-        if(n==1 && arr[0]=='H')
-            return -1;
-        for(int i=0;i<n;i++){
-            if(arr[i] == '.' || arr[i] == 'C') continue;
-            if(arr[i]=='H'){
-                if(i==0 && arr[i+1] != '.')
-                    return -1;
-                if(i==n-1 && arr[i-1] != '.')
-                    return -1;
-                if(i>0 && i<n-1 && arr[i+1]!='.' && arr[i-1]!='.')
-                    return -1;
-            }
-            if(i+2<n && arr[i+1] == '.' && arr[i+2] == 'H'){
-                ans++;
-                arr[i] = 'C';
-                arr[i+2] = 'C';
-                continue;
-            }
-            if(i>0 && arr[i-1] == '.'){
-                ans++;
-                arr[i] = 'C';
-                continue;
-            }
-            if(i<n-1 && arr[i+1] == '.'){
-                ans++;
-                arr[i] = 'C';
-                continue;
+    public int minimumBuckets(String hamsters) {
+        int buckets = 0;
+
+        char[] hams = hamsters.toCharArray();
+
+        for (int i = 0; i < hams.length; i++) {
+            if (hams[i] == 'H') {
+                // Check if hamster is already fed
+                if (i - 1 >= 0 && hams[i - 1] == 'F') {
+                    continue;
+                }
+
+                // Check if we can feed hamster on the next slot
+                if (i + 1 < hams.length && hams[i + 1] == '.') {
+                    hams[i + 1] = 'F';
+                    buckets++;
+                    continue;
+
+                // Check if we can feed hamster on previous slot
+                } else if (i - 1 >= 0 && hams[i - 1] == '.') {
+                    hams[i - 1] = 'F';
+                    buckets++;
+                    continue;
+                }
+
+                return -1;
             }
         }
-        return ans;  
+
+        return buckets;
     }
 }
