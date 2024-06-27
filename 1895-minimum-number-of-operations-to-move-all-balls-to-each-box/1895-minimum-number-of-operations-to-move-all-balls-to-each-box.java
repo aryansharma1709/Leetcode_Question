813df62ab438505e409b08aa6f4ef809abdce44a
aryansharma <aryansharmag29@gmail.com>
2024-06-27 16:56:18 +0530

    Time: 4 ms (83.51%), Space: 44.7 MB (26.72%) - LeetHub

--- 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/1895-minimum-number-of-operations-to-move-all-balls-to-each-box.java
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/1895-minimum-number-of-operations-to-move-all-balls-to-each-box.java
@@ -1,18 +1,31 @@
 class Solution {
-    public int[] minOperations(String boxes) {
-        int []ans =new int[boxes.length()];
-      for(int i=0;i<boxes.length();i++)
-      {
-        int sum=0;
-        for(int j=0;j<boxes.length();j++)
-        {
-             if(boxes.charAt(j)=='1')
-             {
-                sum+=Math.abs(i-j);
-             }
-        } 
-             ans[i]=sum; 
-      }
-      return ans;
+    public int[] minOperations(String s) {
+        int n = s.length();
+        char[] a = s.toCharArray();
+        int[] ans = new int[n];
+        int c = 0;
+        int x = 0;
+        // pref
+        for(int i = 0; i < n; i++){
+            // first initialise
+            ans[i] += x;
+            // System.out.print(" prefix "+ans[i]);
+            if(a[i] == '1')
+                c++;
+            x += c;
+        }
+        // suff
+        c = 0;
+        x = 0;
+        System.out.println();
+        for(int i = n-1; i >= 0; i--){
+            // first initialise
+            ans[i] += x;
+            // System.out.print(" suffix "+ans[i]);
+            if(a[i] == '1')
+                c++;
+            x += c;
+        }
+        return ans;
     }
 }
