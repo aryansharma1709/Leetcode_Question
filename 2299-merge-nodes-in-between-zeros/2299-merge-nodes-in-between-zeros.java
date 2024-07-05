d38c8bc09337b725334138f0148a8009d86d7ef3
aryansharma <aryansharmag29@gmail.com>
2024-07-06 00:43:32 +0530

    Time: 4 ms (100%), Space: 85.3 MB (11.5%) - LeetHub

--- 2299-merge-nodes-in-between-zeros/2299-merge-nodes-in-between-zeros.java
+++ 2299-merge-nodes-in-between-zeros/2299-merge-nodes-in-between-zeros.java
@@ -1,26 +1,24 @@
 public class Solution {
 
     public ListNode mergeNodes(ListNode head) {
-        // Initialize a sentinel/dummy node with the first non-zero value.
-        ListNode modify = head.next;
-        ListNode nextSum = modify;
-
-        while (nextSum != null) {
-            int sum = 0;
-            // Find the sum of all nodes until you encounter a 0.
-            while (nextSum.val != 0) {
-                sum += nextSum.val;
-                nextSum = nextSum.next;
-            }
-
-            // Assign the sum to the current node's value.
-            modify.val = sum;
-            // Move nextSum to the first non-zero value of the next block.
-            nextSum = nextSum.next;
-            // Move modify also to this node.
-            modify.next = nextSum;
-            modify = modify.next;
-        }
-        return head.next;
+       ListNode m=head.next;
+       ListNode temp=m;
+       int sum=0;
+       while(temp!=null)
+       {
+         if(temp.val==0)
+         {
+            m.val=sum;
+            m.next=temp.next;
+            m=m.next;
+            sum=0;
+             
+         }
+         sum+=temp.val;
+         temp=temp.next;
+       }
+       return head.next;
     }
 }
+
+         
