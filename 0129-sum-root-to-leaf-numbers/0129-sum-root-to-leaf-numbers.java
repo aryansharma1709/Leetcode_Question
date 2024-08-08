a60224cbcc209ee79ea0dcabbb12a0985e7f6d9c
aryansharma <aryansharmag29@gmail.com>
2024-08-08 11:27:55 +0530

    Time: 0 ms (100%), Space: 40.7 MB (95.37%) - LeetHub

--- 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.java
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.java
@@ -26,6 +26,7 @@ class Solution {
         int left =number(root.left,sum*10+root.val);
         int right =number(root.right,sum*10+root.val);
         return  left+right;
-
+//  isko hum pre order tracversal se bhi kar skte the usme jab hum leaf node par ho to list me add karte jao aur phir us list ko sum kar lo space use hoga usme.
+//  ye bina space use kiye hai ki ek variable banao aur usko height wise 10 ka multiple hota jayega
     }
 }
