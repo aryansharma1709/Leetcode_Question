e97c61399dede1c9916cda0395b0512aece34afe
aryansharma <aryansharmag29@gmail.com>
2024-08-08 11:01:44 +0530

    Time: 112 ms (100%), Space: 41.7 MB (12.04%) - LeetHub

---
+++ 0988-flip-equivalent-binary-trees/0988-flip-equivalent-binary-trees.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean flipEquiv(TreeNode root1, TreeNode root2) {
+        return flip(root1,root2);
+    }
+    public boolean flip(TreeNode root1, TreeNode root2)
+    {
+        if(root1==null && root2==null)
+        return true;
+        if(root1==null || root2==null)
+        return false;
+        if(root1.val!=root2.val)
+        return false;
+        // mirror  check(Sysmmteric) and equal tree check and return its or 
+        boolean mirror = flip(root1.left ,root2.right) && flip(root1.right ,root2.left);
+        boolean equal_check=flip(root1.left ,root2.left) && flip(root1.right ,root2.right);
+        return mirror || equal_check;
+    }
+}
