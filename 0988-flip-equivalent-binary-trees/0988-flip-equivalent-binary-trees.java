208c6e2e52d8c0f39c11e793f7b2ca759aafafe9
aryansharma <aryansharmag29@gmail.com>
2024-08-08 11:04:25 +0530

    Time: 0 ms (100%), Space: 41.1 MB (66.82%) - LeetHub

--- 0988-flip-equivalent-binary-trees/0988-flip-equivalent-binary-trees.java
+++ 0988-flip-equivalent-binary-trees/0988-flip-equivalent-binary-trees.java
@@ -25,7 +25,7 @@ class Solution {
         return false;
         if(root1.val!=root2.val)
         return false;
-        // mirror  check(Sysmmteric) and equal tree check and return its or 
+        // mirror  check(Sysmmteric) and equal tree check  two easy questn ko mix kar diya  
         boolean mirror = flip(root1.left ,root2.right) && flip(root1.right ,root2.left);
         boolean equal_check=flip(root1.left ,root2.left) && flip(root1.right ,root2.right);
         return mirror || equal_check;
