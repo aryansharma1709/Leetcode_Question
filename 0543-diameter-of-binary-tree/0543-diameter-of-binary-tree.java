72fb8d5bfdfbd0ecfb1cffd95abf874e0dbfad6d
aryansharma <aryansharmag29@gmail.com>
2024-08-14 11:01:27 +0530

    Time: 1 ms (35.05%), Space: 44.7 MB (61.23%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
@@ -15,21 +15,41 @@
  */
 class Solution {
     public int diameterOfBinaryTree(TreeNode root) {
-        return Daimeter(root);
+        return Daimeter(root).d;
     }
-    public int Daimeter(TreeNode root){
-        if(root==null)
-        return 0;
-        int a=Daimeter(root.left);
-        int b=Daimeter(root.right);
-        int sd=height(root.left)+height(root.right)+2;
-        return Math.max(sd,Math.max(a,b));
+    public Daipair Daimeter(TreeNode root)
+    {
+         if(root==null)
+         {
+            return new Daipair();
+         }
+         Daipair ldp= Daimeter(root.left);
+         Daipair rdp =Daimeter(root.right);
+         Daipair sdp=new Daipair();
+         sdp.ht=Math.max(ldp.ht,rdp.ht)+1;
+         int sd=ldp.ht+rdp.ht+2;
+         sdp.d=Math.max(sd,Math.max(ldp.d,rdp.d));
+         return sdp;
     }
-    public int height(TreeNode root){
-        if(root==null)
-        return -1;
-        int l=height(root.left);
-        int r=height(root.right);
-        return Math.max(l,r)+1;
+    class Daipair{
+        int d=0;
+        int ht=-1;
     }
 }
+    // O(n^2) time complexity
+
+    // public int Daimeter(TreeNode root){
+    //     if(root==null)
+    //     return 0;
+    //     int a=Daimeter(root.left);
+    //     int b=Daimeter(root.right);
+    //     int sd=height(root.left)+height(root.right)+2;
+    //     return Math.max(sd,Math.max(a,b));
+    // }
+    // public int height(TreeNode root){
+    //     if(root==null)
+    //     return -1;
+    //     int l=height(root.left);
+    //     int r=height(root.right);
+    //     return Math.max(l,r)+1;
+    // }
