a948a988b49b42a752d898e60c17fcd9c30f14fe
aryansharma <aryansharmag29@gmail.com>
2024-12-30 18:51:20 +0530

    Time: 1 ms (19.25%), Space: 42 MB (21.37%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -3,15 +3,35 @@ class Solution {
         int m=obstacleGrid.length;
         int n=obstacleGrid[0].length;
         int dp[][]=new int[m][n];
-        for(int row[]:dp)
-        Arrays.fill(row,-1);
-        return countPath(dp, obstacleGrid,m-1,n-1);
+       
+        return countPath(dp, obstacleGrid);
     }
-    public int countPath(int [][]dp ,int[][]arr,int i,int j){
-          if(i<0|| j<0)return 0;
-         if(arr[i][j]==1)return 0;
-         if(dp[i][j]!=-1) return dp[i][j];
-         if(i==0 && j==0) return 1;
-         return dp[i][j]=(countPath(dp,arr,i-1,j)+countPath(dp,arr,i,j-1));
+    public int countPath(int [][]dp ,int[][]arr){
+       
+         dp[0][0]=1;
+         for(int i=0;i<dp.length;i++)
+         {
+            for(int j=0;j<dp[0].length;j++)
+            {
+                if(arr[i][j]==1)
+                { dp[i][j]=0;
+                   continue;
+                }
+               if(i==0 && j==0)
+               {
+                dp[0][0]=1;
+                continue;
+               }
+                int down=0;
+                if(i>0)
+                down= dp[i-1][j];
+                int right=0;
+                if(j>0)
+                right=dp[i][j-1];
+                dp[i][j]=down +right;
+                
+            }
+         }
+         return dp[dp.length-1][dp[0].length-1];
     }
 }
